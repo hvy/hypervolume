e7cd94d2ea2b871ca26cb7a491b1e13706d266a5
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2021-01-01 22:14:56 +0900

    Add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-// TODO: Error handling.
 // TODO: Reduce heap allocations.
+// TODO: Document.
 
 pub fn get_hypervolume(pts: &Vec<Vec<f64>>, ref_pt: &Vec<f64>) -> f64 {
+    if ref_pt.is_empty() {
+        panic!("Reference point must have at least one dimension");
+    }
     get_hypervolume_recursive(pts, ref_pt)
 }
 
@@ -91,9 +94,91 @@ mod tests {
     }
 
     #[test]
-    fn test_from_csv() {
-        let filename = "tests/test.csv";
+    fn test_1d_single_point() {
+        let pts = vec![vec![0.3]];
+        let ref_pt = vec![1.0];
+        let hv = 0.7;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
 
+    #[test]
+    fn test_1d_multiple_points() {
+        let pts = vec![vec![0.5], vec![0.3], vec![0.2]];
+        let ref_pt = vec![1.0];
+        let hv = 0.8;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_1d_no_points() {
+        let pts = vec![];
+        let ref_pt = vec![1.0];
+        let hv = 0.0;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_2d_single_point() {
+        let pts = vec![vec![0.3, 0.5]];
+        let ref_pt = vec![1.0, 1.0];
+        let hv = 0.35;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_2d_multiple_points() {
+        let mut pts = vec![vec![0.3, 0.5], vec![0.6, 0.2]];
+        let ref_pt = vec![1.0, 1.0];
+        let hv = 0.47;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+
+        // Non-Pareto optimal points do not contribute to the hypervolume.
+        pts.push(vec![0.8, 0.7]);
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+
+        // Points along the Pareto front does similarly not change the hypervolume.
+        pts.push(vec![0.3, 0.8]);
+        pts.push(vec![0.9, 0.2]);
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_2d_no_points() {
+        let pts = vec![];
+        let ref_pt = vec![1.0, 1.0];
+        let hv = 0.0;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_3d_single_point() {
+        let pts = vec![vec![0.5, 0.5, 0.5]];
+        let ref_pt = vec![1.0, 1.0, 1.0];
+        let hv = 0.125;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_3d_no_points() {
+        let pts = vec![];
+        let ref_pt = vec![1.0, 1.0, 1.0];
+        let hv = 0.0;
+        let hv_computed = get_hypervolume(&pts, &ref_pt);
+        assert!(is_float_close(hv_computed, hv));
+    }
+
+    #[test]
+    fn test_3d_from_csv() {
+        let filename = "tests/3d.csv";
         let contents = std::fs::read_to_string(filename).unwrap();
 
         for line in contents.lines() {
@@ -129,14 +214,15 @@ mod tests {
             let hv: f64 = hv_str.parse().unwrap();
 
             let hv_computed = get_hypervolume(&pts, &ref_pt);
-
-            assert!(
-                is_float_close(hv, hv_computed),
-                "Computed: {}, Expected: {}, Err: {}",
-                hv_computed,
-                hv,
-                hv - hv_computed
-            );
+            assert!(is_float_close(hv_computed, hv));
         }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_invalid_reference_point() {
+        let pts = vec![vec![0.5, 0.5]];
+        let ref_pt = vec![];
+        let _ = get_hypervolume(&pts, &ref_pt);
     }
 }
